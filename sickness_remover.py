ab04786c0a101ca46ca189a5195fbb5fa3503684
JUKjacker <80601981+MrWitzbold@users.noreply.github.com>
2021-06-16 16:54:58 -0300

    just let it go

--- sickness_remover.py
+++ sickness_remover.py
@@ -2,14 +2,17 @@ from earth import humans
 from earth import sicknesses
 
 for human in humans:
-    human.is_sick = False
-    human.has_antibodies = True
+    human.is_sick = True
+    human.has_antibodies = False
 
 for sickness in sicknesses:
-    sickness.active = False
-    sickness.has_cure = True
+    sickness.active = True
+    sickness.has_cure = False
     
 
       
 humans.need_to_mask = False
 humans.need_to_quarantine = False
+
+israel_is_the_little_horn = True
+Start_end_days(israel_is_the_little_horn)
